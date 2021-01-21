2223e489c14d609f37b28d5fa44b4ba08603b155
ClementBuchanan <clement.buchanan@gmail.com>
2021-01-20 18:56:50 -0800

    completed lab 06

--- server.js
+++ server.js
@@ -39,39 +39,60 @@ app.get('/', (req, res) => {
 // this lives with all the client data, in the `request (req)` parameter
 // inside request will always live a property  query: { name: 'rasta', lastName: 'dog' },
 
+// ====== location request ======
 
 // create a route with an end point of location
 // location request/send
-
 app.get('/location', (req, res) => {
-//   res.send({
-//     'search_query': 'seattle',
-//     'formatted_query': 'Seattle, WA, USA',
-//     'latitude': '47.606210',
-//     'longitude': '-122.332071'
-//   });
-  if (!req.query.city) {
-    res.status(500).send('Sorry, something went terribly wrong');
-  }
+
+
+  // we need to normalize our data with a constructor
+  const newLocation = require('./data/location.json'); // require gets things from a fle
+  const location = newLocation[0];
+
+  // data from the client (search query they submitted)
+  console.log('req.query', req.query);
+
+  const searchedCity = req.query.city;
+  const site = new Location(searchedCity,
+    location.display_name,
+    location.lat,
+    location.lon
+  );
+  res.send(site);
 });
 
+// res.send({
+//   'search_query': 'seattle',
+//   'formatted_query': 'Seattle, WA, USA',
+//   'latitude': '47.606210',
+//   'longitude': '-122.332071'
+// });
+// if (!req.query.city) {
+//   res.status(500).send('Sorry, something went terribly wrong');
+// };
+
 // ====== weather request/send ======
+
 app.get(`/weather`, (req, res) => {
-  const dummyWeatherData = [
-    {
-      "forecast": 'Its going to rain like it has never rained before.',
-      "time": 'Monday January 18 2021',
-    },
-    {
-      "forecast": 'Its going to be cloudy with a chance of meatballs.',
-      "time": 'Tuesday January 19 2021',
-    },
-  ];
+  
+  const dummyWeatherData = require('./data/weather.json'); // require gets things from a fle
+
+  
+  
+  // const dummyWeatherData = [
+  //   {
+  //     'forecast': 'Its going to rain like it has never rained before.',
+  //     'time': 'Monday January 18 2021',
+  //   },
+  //   {
+  //     'forecast': 'Its going to be cloudy with a chance of meatballs.',
+  //     'time': 'Tuesday January 19 2021',
+  //   },
+  // ];
   // ====== return weather object ======
-  const arr = [];
-  dummyWeatherData.forEach(jsonObj => {
-    const weather = new weather(jsonObj);
-    arr.push(weather);
+  const arr = dummyWeatherData.data.map(jsonObj => {
+    return new Weather(jsonObj.weather.description, jsonObj.ts);
   });
   res.send(arr);
 });
@@ -80,8 +101,14 @@ app.get(`/weather`, (req, res) => {
 
 function Location(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
+  this.formatted_query = formatted_query;
   this.latitude = latitude;
   this.longitude = longitude;
+}
+
+function Weather(forecast, time) {
+  this.forecast = forecast;
+  this.time = new Date(time * 1000).toDateString ();
 }
 
 // ===== Start the server =====
