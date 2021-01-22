042917bd416b47a3ee7f58e6a67631f0a07caf82
ClementBuchanan <clement.buchanan@gmail.com>
2021-01-21 19:37:10 -0800

    updated server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require('express');
 const cors = require('cors');
 // const { response } = require('express');
 require('dotenv').config();
+const superagent = require('superagent');
 
 // =====setup application (server) ========
 
@@ -19,7 +20,7 @@ app.use(cors());
 // ===== Global variables =====
 // process.env.PORT references the the PORT env variable from the terminal
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 2021;
 
 // ===== Routes =====
 // app.get: attaches a listener of method type GET to the server with a (route and a callback)
@@ -34,81 +35,113 @@ app.get('/', (req, res) => {
 });
 
 
-// expect a key value pair of name:rasta and lastName:dog
-// send `rasta dog`
-// this lives with all the client data, in the `request (req)` parameter
-// inside request will always live a property  query: { name: 'rasta', lastName: 'dog' },
+// ====== superagent & location request/send  ======
 
-// ====== location request ======
+function locationHandler(req, res) {
+  const searchedCity = req.query.city;
+  // console.log(req.query);
+  const key = process.env.LOCATION_API_KEY;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${searchedCity}&format=json`;
+
+  superagent.get(url)
+    .then(result => {
+    // console.log(result.body[0]);
+    const newLocation = new Location(result.body[0], searchedCity);
+    console.log(newLocation);
+      // const location = result.body[0];
+      // const newLocation = new Location(
+      //   searchedCity,
+      //   location.display_name,
+      //   location.lat,
+      //   location.lon
+      // );
+      res.send(newLocation);
+    })
+    .catch(error => {
+      res.status(500).send('please enter a city in the search field');
+      // console.log(error);
+    });
+}
+
+app.get('/location', (req, res) => locationHandler(req, res));
 
 // create a route with an end point of location
 // location request/send
-app.get('/location', (req, res) => {
+// app.get('/location', (req, res) => {
+//   // we need to normalize our data with a constructor
+//   const newLocation = require('./data/location.json'); // require gets
+//   const location = newLocation[0];
 
+//   // data from the client (search query they submitted)
+//   console.log('req.query', req.query);
 
-  // we need to normalize our data with a constructor
-  const newLocation = require('./data/location.json'); // require gets things from a fle
-  const location = newLocation[0];
-
-  // data from the client (search query they submitted)
-  console.log('req.query', req.query);
-
-  const searchedCity = req.query.city;
-  const site = new Location(searchedCity,
-    location.display_name,
-    location.lat,
-    location.lon
-  );
-  res.send(site);
-});
-
-// res.send({
-//   'search_query': 'seattle',
-//   'formatted_query': 'Seattle, WA, USA',
-//   'latitude': '47.606210',
-//   'longitude': '-122.332071'
+//   const searchedCity = req.query.city;
+//   const site = new Location(searchedCity,
+//     location.display_name,
+//     location.lat,
+//     location.lon
+//   );
+//   res.send(site);
 // });
-// if (!req.query.city) {
-//   res.status(500).send('Sorry, something went terribly wrong');
-// };
 
-// ====== weather request/send ======
+// ====== weather superagent & request/send ======
 
 app.get(`/weather`, (req, res) => {
-  
-  const dummyWeatherData = require('./data/weather.json'); // require gets things from a fle
-
-  
-  
-  // const dummyWeatherData = [
-  //   {
-  //     'forecast': 'Its going to rain like it has never rained before.',
-  //     'time': 'Monday January 18 2021',
-  //   },
-  //   {
-  //     'forecast': 'Its going to be cloudy with a chance of meatballs.',
-  //     'time': 'Tuesday January 19 2021',
-  //   },
-  // ];
-  // ====== return weather object ======
-  const arr = dummyWeatherData.data.map(jsonObj => {
-    return new Weather(jsonObj.weather.description, jsonObj.ts);
-  });
-  res.send(arr);
+  const key = process.env.WEATHER_API_KEY
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+  const url = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=${latitude}&lon=${longitude}&hours=24&key=${key}&units=I`;
+  superagent.get(url)
+    .then(result => {
+      function Weather(forecast, time) {
+        this.forecast = forecast;
+        this.time = new Date(time * 1000).toDateString();
+      }
+    });
 });
+
+// ====== Trails request/send ======
+
+app.get(`/parks`, (req, res) => {
+  const key = process.env.PARKS_API_KEY;
+  const parkName = req.query.parkName;
+  const parkLocation = req.query.parkLocation;
+  console.log('000000000', req.query);
+  const url = `https://${key}@developer.nps.gov/api/v1/parks?parkCode=acad?${parkName}&parkLocation=${parkLocation}`;
+  superagent.get(url)
+    .then(result => {
+      result.body.data.map(parkResults => {
+        // console.log(parkResults);
+      })
+      // console.log(result.body.data, '!!!!!!!!!!!!!!!!!');
+      // const parks = new Park(result);
+      // function Park(parkName, parkLocation) {
+      //   this.parkName = park_name;
+      //   this.parkLocation = park_location;
+      // }
+    });
+});
+
 
 // ===== Helper functions =====
 
-function Location(search_query, formatted_query, latitude, longitude) {
-  this.search_query = search_query;
-  this.formatted_query = formatted_query;
-  this.latitude = latitude;
-  this.longitude = longitude;
+function Location(obj, searchedCity) {
+  this.search_query = searchedCity;
+  this.display_name = obj.display_name;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
 }
 
 function Weather(forecast, time) {
   this.forecast = forecast;
-  this.time = new Date(time * 1000).toDateString ();
+  this.time = new Date(time * 1000).toDateString();
+}
+
+function Park(search_query, formatted_query, park_name, park_location) {
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.parkName = park_name;
+  this.parkLocation = park_location;
 }
 
 // ===== Start the server =====
